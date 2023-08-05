05592aae7e28a0d370fc9ca81eab348192af19dd
MMqd <u@pc.localdomain>
2023-08-05 18:31:59 -0400

    Changed to new QMK key names, and added some duplicate keys to make some shortcuts easier

--- keymap.c
+++ keymap.c
@@ -17,26 +17,32 @@
 #define LSA_T(kc) MT(MOD_LSFT | MOD_LALT, kc)
 #define BP_NDSH_MAC ALGR(KC_8)
 
+enum layers {
+    BASE,  // default layer
+    SYMB,  // symbols
+    MDIA,  // media keys
+};
+
 enum custom_keycodes {
-  RGB_SLD = EZ_SAFE_RANGE,
+    VRSN = SAFE_RANGE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,                                KC_TRANSPARENT,      KC_6,       KC_7,       KC_8,       KC_9,       KC_0,           KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_Q,       KC_W,       KC_F,       KC_P,       KC_G,       TG(3),                               TG(4),               KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCOLON,      KC_TRANSPARENT,
+    KC_TAB, KC_Q,       KC_W,       KC_F,       KC_P,       KC_G,       TG(3),                               TG(4),               KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,      KC_TRANSPARENT,
     KC_LSFT,        KC_A,       KC_R,       KC_S,       KC_T,       KC_D,                                                                 KC_H,       KC_N,       KC_E,       KC_I,       KC_O,           KC_TRANSPARENT,
     KC_LCTL,        KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_TRANSPARENT,                      KC_TRANSPARENT,      KC_K,       KC_M,       KC_COMMA,   KC_DOT,     KC_SLASH,       KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LGUI_T(KC_TAB), LALT_T(KC_DEL),                                                     LGUI_T(KC_TAB), LALT_T(KC_DEL), KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_LALT, KC_TRANSPARENT, KC_TRANSPARENT, LGUI_T(KC_TAB), LALT_T(KC_DEL),                                                     LGUI_T(KC_TAB), LALT_T(KC_DEL), KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                              KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                              KC_TRANSPARENT, KC_TRANSPARENT,
-                                                           LT(2,KC_SPACE), LCTL_T(KC_ESCAPE),KC_TRANSPARENT, KC_TRANSPARENT, LSFT_T(KC_ENTER),LT(1,KC_BSPACE)
+                                                           LT(2,KC_SPACE), LCTL_T(KC_ESCAPE),KC_TRANSPARENT, KC_TRANSPARENT, LSFT_T(KC_ENTER),LT(1,KC_BSPC)
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,                                          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,         KC_TRANSPARENT,
     KC_TRANSPARENT, KC_6,           KC_4,           KC_2,           KC_0,           KC_8,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_9,           KC_1,           KC_3,           KC_5,           KC_7,           KC_TRANSPARENT,
     KC_TRANSPARENT, KC_EXLM,        KC_HASH,        KC_QUOTE,       KC_PERC,        KC_PIPE,                                                                        KC_PLUS,        KC_MINUS,       KC_DQUO,        KC_AMPR,        KC_EQUAL,       KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_UNDS,        KC_AT,          KC_CIRC,        KC_TILD,        KC_NO,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_NO,          KC_ASTR,        KC_DLR,         KC_GRAVE,       KC_BSLASH,      KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_UNDS,        KC_AT,          KC_CIRC,        KC_TILD,        KC_NO,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_NO,          KC_ASTR,        KC_DLR,         KC_GRAVE,       KC_BSLS,      KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                              KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                              KC_TRANSPARENT, KC_TRANSPARENT,
@@ -44,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [2] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_LCBR,        KC_LBRACKET,    KC_LABK,        KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_RABK,        KC_RBRACKET,    KC_RCBR,        KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_LCBR,        KC_LBRC,    KC_LABK,        KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_RABK,        KC_RBRC,    KC_RCBR,        KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,       KC_MS_RIGHT,    KC_WWW_BACK,                                                                    KC_WWW_FORWARD, KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT,
-    KC_TRANSPARENT, LCTL_T(KC_HOME),LSFT_T(KC_END), LCTL_T(KC_PGDOWN),LSFT_T(KC_PGUP),KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_WH_DOWN,  KC_MS_WH_UP,    KC_MS_ACCEL2,   KC_MS_ACCEL0,   KC_TRANSPARENT,
+    KC_TRANSPARENT, LCTL_T(KC_HOME),LSFT_T(KC_END), LCTL_T(KC_PGDN),LSFT_T(KC_PGUP),KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_WH_DOWN,  KC_MS_WH_UP,    KC_MS_ACCEL2,   KC_MS_ACCEL0,   KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_MS_BTN3, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                             KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                             KC_TRANSPARENT, KC_TRANSPARENT,
@@ -55,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [3] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_6,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
-    KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_TRANSPARENT,
-    KC_LCTRL,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_Y,                                 KC_TRANSPARENT, KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT,
+    KC_LSFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,      KC_TRANSPARENT,
+    KC_LCTL,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_Y,                                 KC_TRANSPARENT, KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT,
     KC_RALT,         KC_H, KC_J, KC_K, KC_ESCAPE,                                                                                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                              KC_U          , KC_I          , KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                              KC_O          , KC_TRANSPARENT,
@@ -74,49 +80,89 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-  }
-  return true;
+    if (record->event.pressed) {
+        switch (keycode) {
+            case VRSN:
+                SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+                return false;
+        }
+    }
+    return true;
 }
 
-uint32_t layer_state_set_user(uint32_t state) {
-  uint8_t layer = biton32(state);
-  ergodox_board_led_off();
-  ergodox_right_led_1_off();
-  ergodox_right_led_2_off();
-  ergodox_right_led_3_off();
-  ergodox_led_all_set(LED_BRIGHTNESS_LO);
-  switch (layer) {
-    case 1:
-      ergodox_right_led_1_on();
-      break;
-    case 2:
-      ergodox_right_led_2_on();
-      break;
-    case 3:
-      ergodox_right_led_3_on();
-      break;
-    case 4:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      break;
-    case 5:
-      ergodox_right_led_1_on();
-      ergodox_right_led_3_on();
-      break;
-    case 6:
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
-      break;
-    case 7:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
-      break;
-    default:
-      break;
-  }
-  return state;
+// Runs just one time when the keyboard initializes.
+void keyboard_post_init_user(void) {
+#ifdef RGBLIGHT_COLOR_LAYER_0
+    rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+#endif
+};
+
+// Runs whenever there is a layer state change.
+layer_state_t layer_state_set_user(layer_state_t state) {
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    ergodox_led_all_set(LED_BRIGHTNESS_LO);
+
+    uint8_t layer = get_highest_layer(state);
+    switch (layer) {
+        case 0:
+#ifdef RGBLIGHT_COLOR_LAYER_0
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+#endif
+            break;
+        case 1:
+            ergodox_right_led_1_on();
+#ifdef RGBLIGHT_COLOR_LAYER_1
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+#endif
+            break;
+        case 2:
+            ergodox_right_led_2_on();
+#ifdef RGBLIGHT_COLOR_LAYER_2
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+#endif
+            break;
+        case 3:
+            ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_3
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+#endif
+            break;
+        case 4:
+            ergodox_right_led_1_on();
+            ergodox_right_led_2_on();
+#ifdef RGBLIGHT_COLOR_LAYER_4
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+#endif
+            break;
+        case 5:
+            ergodox_right_led_1_on();
+            ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_5
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+#endif
+            break;
+        case 6:
+            ergodox_right_led_2_on();
+            ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_6
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+#endif
+            break;
+        case 7:
+            ergodox_right_led_1_on();
+            ergodox_right_led_2_on();
+            ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_7
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_7);
+#endif
+            break;
+        default:
+            break;
+    }
+
+    return state;
 };
